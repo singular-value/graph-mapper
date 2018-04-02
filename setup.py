f8820ca198819930a068877e8adac6a3a8f94f84
Pranav Gokhale <pranav@telepath.fyi>
2018-04-01 21:05:16 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
-with open('README', 'r') as f:
+with open('README.rst', 'r') as f:
     readme = f.read()
 
-setup(name='data_mapper',
-      version='0.2',
-      description='Uses graph partitioning techniques to assign data to physical locations',
+setup(name='graph_mapper',
+      version='1.0.dev1',
+      description='Uses graph partitioning techniques to assign data to locations on a rectangle',
       long_description=readme,
-      url='http://github.com/singular-value/data_mapper',
+      url='http://github.com/singular-value/graph_mapper',
       author='Pranav Gokhale',
       author_email='pranavgokhale@uchicago.edu',
       license='MIT',
-      packages=['data_mapper'],
+      packages=['graph_mapper'],
+      install_requires=['metis'],
+      python_requires='>=3',
       zip_safe=False)
